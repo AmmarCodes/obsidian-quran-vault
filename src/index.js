77fb7bd8eebdd0dd1e622d6d1e31cede0a8b6a29
Ammar Alakkad <am.alakkad@gmail.com>
2021-12-12 21:49:53 +0300

    Create Arabic Quran obisidian vault

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const fs = require("fs-extra");
+
+const VAULT_PATH = "./Quran/";
+
+const quranJsonPath = require.resolve("quran-json/dist/quran.json");
+
+const surahs = fs.readJsonSync(quranJsonPath);
+
+surahs.forEach((surah) => {
+  const surahTitle = surah.transliteration;
+  const surahFolderPath = `${VAULT_PATH}${surahTitle}`;
+  fs.ensureDir(surahFolderPath, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // creating files inside directory
+    surah.verses.forEach((verse) => {
+      const verseFilePath = `${surahFolderPath}/${verse.id}.md`;
+      const content = `---\ntags: #quran #${surahTitle}\n---\n\n> ${verse.text}\n`;
+      fs.outputFile(verseFilePath, content, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+
+    // the following will create `index.md` file in each surah folder containing an internal link to each verse
+    /**
+    let indexContent = `# ${surahTitle}\n\n`;
+    [...Array(surah.verses.length)].map((_, i) => {
+      // since i will start at 0
+      const verseId = i + 1;
+      indexContent += `- [[${surahTitle}/${verseId}]]\n`;
+    });
+
+    fs.outputFile(`${VAULT_PATH}${surahTitle}/index.md`, indexContent);
+    */
+  });
+});
